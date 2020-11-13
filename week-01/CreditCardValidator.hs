ad91f2adc1084dfbbdb93cd0bee3a5dee705b57c
aykut <aykut@jotform.com>
2020-11-13 23:32:25 +0300

    Week1 - CreditCardValidator refactoring

--- week-01/CreditCardValidator.hs
+++ week-01/CreditCardValidator.hs
@@ -1,30 +1,22 @@
 module CreditCardValidator where
 
-import Data.Char (digitToInt)
-
 -- Exercise 1
 -- toDigits 1234 == [1,2,3,4]
 toDigits :: Integer -> [Integer]
-toDigits x
-  | x <= 0 = []
-  | otherwise = map (toInteger . digitToInt) . show $ x
+toDigits = go [] . abs
+  where
+    go acc 0 = acc
+    go acc x = go (snd b : acc) (fst b) where b = x `divMod` 10
 
 -- Exercise 2
 -- doubleEveryOther [1,2,3] == [1,4,3]
 -- doubleEveryOther [1,2,3,4] == [2,2,6,4]
 -- Double every other number starting from the right
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = reverse . (go 0 . reverse)
-  where
-    go i (x:xs)
-      | null xs = [dbl i x]
-      | otherwise = dbl i x : go (i + 1) xs
-
-dbl :: Integer -> Integer -> Integer
-dbl x y
-  | odd x = y * 2
-  | otherwise = y
--- It should be a better way to do this.
+doubleEveryOther [] = []
+doubleEveryOther l@(x:xs)
+  | odd (length l) = x : doubleEveryOther xs
+  | otherwise = x * 2 : doubleEveryOther xs
 
 -- https://bit.ly/3eJ3va9
 doubleEveryOther' :: [Integer] -> [Integer]
